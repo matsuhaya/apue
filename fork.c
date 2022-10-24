4ad69d375e90051cc034ff6e83c539ead0bee0df
matsuhaya <matsuhaya007@gmail.com>
2022-10-24 09:14:17 +0900

    add sig_int

--- fork.c
+++ fork.c
@@ -2,10 +2,14 @@
 
 #include "apue.h"
 
+static void sig_int(int);
+
 int main(void) {
   char buf[MAXLINE];
   pid_t pid;
   int status;
+
+  if (signal(SIGINT, sig_int) == SIG_ERR) err_sys("sygnal error");
 
   printf("%% ");
   while (fgets(buf, MAXLINE, stdin) != NULL) {
@@ -26,3 +30,5 @@ int main(void) {
   }
   exit(0);
 }
+
+void sig_int(int signo) { printf("interrupt\n%% "); }
