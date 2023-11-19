231f85a2629d5aaf4a0b8ce2ce2674d4c8c0b1f9
matsuhaya <matsuhaya007@gmail.com>
2023-11-19 15:26:59 +0900

    update waitpid.c

--- waitpid.c
+++ waitpid.c
@@ -8,6 +8,7 @@ int main(void) {
   if ((pid = fork()) < 0) {
     err_sys("fork error");
   } else if (pid == 0) {
+    // (*)fork_8.3.cで確認した通り、子プロセスは親プロセスの変数を変更更新できない
     if ((pid = fork()) < 0)
       err_sys("fork error");
     else if (pid > 0)
@@ -18,6 +19,7 @@ int main(void) {
     exit(0);
   }
 
+  // (*)より、ここでのpidは子プロセスのpid（孫ではない）
   if (waitpid(pid, NULL, 0) != pid) err_sys("waitpid error");
 
   exit(0);
