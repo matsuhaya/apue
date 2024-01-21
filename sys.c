3480c97277ce63d1db0e2507ad545198a38b081b
matsuhaya <matsuhaya007@gmail.com>
2024-01-21 14:12:37 +0900

    add sys.c

---
+++ sys.c
@@ -0,0 +1,27 @@
+#include <sys/wait.h>
+
+#include "apue.h"
+
+int main(void) {
+  int status;
+
+  if ((status = system("date")) < 0) {
+    err_sys("system() error");
+  }
+
+  pr_exit(status);
+
+  if ((status = system("nosuchcommand")) < 0) {
+    err_sys("system() error");
+  }
+
+  pr_exit(status);
+
+  if ((status = system("who; exit 44")) < 0) {
+    err_sys("system() error");
+  }
+
+  pr_exit(status);
+
+  exit(0);
+}
